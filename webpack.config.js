056d6b1700debd276542fe4563bfd1ad15db3866
Mehdi ElHaij <mehdi.elhaij@outlook.com>
2022-06-10 01:45:48 +0100

    webpack: support svg

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack'],
+      },
+      {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ["babel-loader"],
