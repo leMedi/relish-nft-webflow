d95e01b1954c54db08bcdac7ef2cae6fcdda66c7
Mehdi ElHaij <mehdi.elhaij@outlook.com>
2022-07-06 16:42:22 +0200

    fix total eth raised

--- src/EthRaised.jsx
+++ src/EthRaised.jsx
@@ -8,11 +8,15 @@ export const EthRaised = () => {
 
   useEffect(() => {
     if (!mintInfo) return;
+    console.log('EthRaised mintInfo', mintInfo);
     const [l1Count, l1Price, l2Count, l2Price, l3Count, l3Price] = mintInfo;
+    console.log("EthRaised l1Count", l1Count.toNumber());
+    console.log("EthRaised l1Price", l1Price.toString());
+
     const totalEthRaised =
       l1Count * l1Price + l2Count * l2Price + l3Count * l3Price;
     console.log("loadData totalEthRaised", totalEthRaised);
-    setEthRaised(ethers.utils.formatEther(totalEthRaised));
+    setEthRaised(ethers.utils.formatEther(totalEthRaised.toString()));
   }, [mintInfo]);
 
   return <div className="text-block-21">{ethRaised}</div>;
